a8a69eb537fb94dd8a6866fbfa29edbd2053a63d
Pedro Sena <pmfsl@cin.ufpe.br>
2023-05-12 12:13:28 -0300

    criar teste de adicionar pokemon na pokedex

--- pokedex/main.py
+++ pokedex/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 
 class Pokemon:
     def __init__(self, id, nome, altura, peso, habilidades):
@@ -9,9 +11,10 @@ class Pokemon:
 
 
 class Pokedex:
-    def __init__(self, pokemon: Pokemon = None):
-        self.pokemon = None
-        self.pokemons = []
+    def __init__(self):
+        self.pokemons: List[Pokemon] = list()
 
-    def adiciona_pokemon(self, pokemon):
-        ...
+    def adiciona_pokemon(self, pokemon: Pokemon):
+        # TODO: Mudar verificação do pokemon na lista
+        if pokemon not in self.pokemons:
+            self.pokemons.append(pokemon)
--- pokedex/test.py
+++ pokedex/test.py
@@ -1,12 +1,6 @@
 import unittest
 
-from main import Pokemon
-
-
-class TestCase(unittest.TestCase):
-    def test_example(self):
-        """Should be a test case"""
-        self.assertEqual(1, 1)
+from main import Pokedex, Pokemon
 
 
 class TestPokemon(unittest.TestCase):
@@ -19,8 +13,53 @@ class TestPokemon(unittest.TestCase):
             habilidades=['Tomar Monster',
                          'Cuspir fogo', 'Investida', 'Teleport']
         )
+
         self.assertEqual(1, pokemon.id)
         self.assertTrue(pokemon)
         self.assertEqual('Evaristo/INSS', pokemon.nome)
         self.assertEqual(3.00, pokemon.altura)
         self.assertEqual(50, pokemon.peso)
+
+
+class TestPokedex(unittest.TestCase):
+    def test_adicionar_pokemon(self):
+        # TODO: procurar fução beforeEach
+        pokedex = Pokedex()
+        pokemon = Pokemon(
+            id=1,
+            nome='Evaristo/INSS',
+            altura=3.00,
+            peso=50,
+            habilidades=['Tomar Monster',
+                         'Cuspir fogo', 'Investida', 'Teleport']
+        )
+
+        pokemon2 = Pokemon(
+            id=1,
+            nome='Eva',
+            altura=3.60,
+            peso=500,
+            habilidades=['Tomar red bull',
+                         'Cuspir fogo', 'Investida', 'Teleport']
+        )
+
+        pokedex.adiciona_pokemon(pokemon)
+        pokedex.adiciona_pokemon(pokemon2)
+        self.assertIn(pokemon, pokedex.pokemons)
+
+        self.assertEqual(len(pokedex.pokemons), 1)
+
+    def test_procurar_pokemon(self):
+        pokedex = Pokedex()
+        pokemon = Pokemon(
+            id=1,
+            nome='Evaristo/INSS',
+            altura=3.00,
+            peso=50,
+            habilidades=['Tomar Monster',
+                         'Cuspir fogo', 'Investida', 'Teleport']
+        )
+
+        pokedex.adiciona_pokemon(pokemon)
+
+        self.assertIn(pokemon, pokedex.pokemons)
